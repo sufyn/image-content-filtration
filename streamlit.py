2c8df99e4641bd8fca24e4ce7eab98c18fe79aa0
sufyaan <97327266+sufyn@users.noreply.github.com>
2024-07-03 11:21:14 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PIL import Image
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 import numpy as np
 from io import BytesIO
 import requests
@@ -11,11 +10,11 @@ import requests
 model = load_model('model/img_model_1.1.h5')
 
 # Predict the class
-def predict(image):
-    image = image.resize((128, 128))
-    image = np.array(image) / 255.0
-    image = np.expand_dims(image, axis=0)
-    prediction = model.predict(image)
+def predict(img):
+    img = img.resize((128, 128))
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+    prediction = model.predict(img)
     return np.argmax(prediction, axis=1)[0], prediction
 
 st.title("Image Classification App")
@@ -28,36 +27,37 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 # Input image URL
 image_url = st.text_input("Or enter an image URL:")
 
-if uploaded_file is not None:
-    img = Image.open(uploaded_file)
-    if img.mode == 'RGBA':
-                img = img.convert('RGB')
-
-    st.image(img, caption="Uploaded Image", use_column_width=True)
-    label, prediction = predict(img)
-    categories = ['Adult Content', 'Safe', 'Violent']
-    predicted_category = categories[label]
-    st.write(f"Classification: {predicted_category}")
-    st.write(f"Confidence: {float(prediction[0][label]):.2f}")
-
-elif image_url:
-    try:
-        response = requests.get(image_url, stream=True)
-        if response.status_code == 200:
-            response.raw.decode_content = True
-            img = Image.open(BytesIO(response.content))
+# Button to trigger image URL submission and classification
+if st.button("Submit"):
+    if uploaded_file is not None:
+        img = Image.open(uploaded_file)
+        if img.mode == 'RGBA':
+            img = img.convert('RGB')
             
-            if img.mode == 'RGBA':
-                img = img.convert('RGB')
-            else:
+        st.image(img, caption="Uploaded Image", use_column_width=True)
+        label, prediction = predict(img)
+        categories = ['Adult Content', 'Safe', 'Violent']
+        predicted_category = categories[label]
+        st.write(f"Classification: {predicted_category}")
+        st.write(f"Confidence: {float(prediction[0][label]):.2f}")
+
+    elif image_url:
+        try:
+            response = requests.get(image_url, stream=True)
+            if response.status_code == 200:
+                response.raw.decode_content = True
+                img = Image.open(BytesIO(response.content))
+                
+                if img.mode == 'RGBA':
+                    img = img.convert('RGB')
+                
                 st.image(img, caption="Fetched Image", use_column_width=True)
                 label, prediction = predict(img)
                 categories = ['Adult Content', 'Safe', 'Violent']
                 predicted_category = categories[label]
                 st.write(f"Classification: {predicted_category}")
                 st.write(f"Confidence: {float(prediction[0][label]):.2f}")
-        else:
-            st.error("Failed to retrieve image from the provided URL")
-    except Exception as e:
-        st.error(f"Error fetching image: {e}")
-
+            else:
+                st.error("Failed to retrieve image from the provided URL")
+        except Exception as e:
+            st.error(f"Error fetching image: {e}")
