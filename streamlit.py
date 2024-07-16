ecaea1c4affedb271e5fd4b725870ed0c092b008
sufyn <sufyaan4guys@gmail.com>
2024-07-16 18:06:25 +0530

    update

--- streamlit.py
+++ streamlit.py
@@ -25,6 +25,8 @@ ageNet = cv2.dnn.readNet(ageModel, ageProto)
 classificationModel = tf.keras.models.load_model(classificationModelPath)
 
 # Load FastAI model
+def label_func(f): return f.parent.name
+
 fastai_model_path = 'model/img_model2.pkl'
 fastai_model = fastai.vision.learner.load_learner(fastai_model_path)
 
